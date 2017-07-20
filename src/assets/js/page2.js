9e61babe1a6bc4b4915ae80b818f546d8848ec70
sonalidamute <sonalidamute@gmail.com>
2017-07-20 18:42:37 +0530

    page navigation

--- src/assets/js/page2.js
+++ src/assets/js/page2.js
@@ -1,9 +1,24 @@
 var app = angular.module("app");
 
 
-app.controller("page2", function($scope){
+app.controller("page2", function($scope, $location){
 
     // CONCEPT.
     console.log("PAGE2");
+
+    $scope.data1 = {"title" : "Hello Page2"};
+
+    $scope.dataList = [
+        {"title":"page1", "color": "red"}, 
+        {"title":"page2", "color" : "green"}
+    ];
+    
+    
+    $scope.goToPage1 = function(){
+
+        console.log("GO TO PAGE1");
+
+        $location.path("/page1");
+    };
 
 });
